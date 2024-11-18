5615b94b60c9d80a093290719e95258ad9a8c9e4
SaintJachriz <mbagwujachi03@gmail.com>
2024-11-19 03:15:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Show(){
+    const hidden = document.querySelector('.mobile-view');
+    hidden.style.display = 'flex';
+}
+
+function Delete(){
+    const hidden = document.querySelector('.mobile-view');
+    hidden.style.display = 'none';
+}
